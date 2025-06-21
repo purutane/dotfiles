24345702965d588211dc536cb436a5ab69d8088c
purutane <8597192+purutane@users.noreply.github.com>
2025-06-21 22:02:45 +0900

    refactor(nvim): improve code organization and consistency in options.lua
    - Consolidate encoding settings and remove duplication
    - Reorganize sections with clearer comments
    - Switch from vim.o to opt for consistency
    - Improve code structure and readability

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,53 +1,51 @@
 local opt = vim.opt
 
--- encodings
-vim.o.encoding = 'utf-8'
-vim.o.fileencoding = 'utf-8'
+-- Encoding settings
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
 vim.o.fileencodings = 'utf-8,iso-2022-jp,euc-jp,sjis'
+
+-- Locale settings
 vim.o.langmenu = 'ja_JP.UTF-8'
 vim.o.helplang = 'ja'
 
--- 基本設定
+-- Line numbers
 opt.number = true
 opt.relativenumber = true
-opt.mouse = ''
-opt.clipboard = ''
 
--- インデントとタブ
+-- Input settings
+opt.mouse = ''
+opt.clipboard = 'unnamedplus'
+
+-- Indentation and tabs
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 opt.smartindent = true
 
--- 検索関連
+-- Search settings
 opt.ignorecase = true
 opt.smartcase = true
 opt.hlsearch = true
 opt.incsearch = true
 
--- UI関連
+-- UI settings
 opt.wrap = true
 opt.scrolloff = 8
 opt.sidescrolloff = 8
 opt.termguicolors = true
 opt.showmode = false
 
--- ファイル関連
-opt.encoding = 'utf-8'
-opt.fileencoding = 'utf-8'
+-- File handling
 opt.backup = false
 opt.writebackup = false
 opt.swapfile = false
 
--- アンドゥ関連
+-- Undo settings
 opt.undofile = true
--- アンドゥ履歴の保存場所
 local undodir = vim.fn.stdpath('data') .. '/undodir'
 if vim.fn.isdirectory(undodir) == 0 then
   vim.fn.mkdir(undodir, 'p')
 end
 opt.undodir = undodir
-
--- clipboard
-opt.clipboard = 'unnamedplus'
