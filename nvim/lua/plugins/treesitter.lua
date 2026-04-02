6644eb1ffa4b8d133be23c11d21863e72e3e52d8
purutane <8597192+purutane@users.noreply.github.com>
2026-04-02 13:04:01 +0900

    refactor(nvim): migrate treesitter to new modular API

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,81 +1,48 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
-    event = { 'BufReadPost', 'BufNewFile' },
-    cmd = {
-      'TSInstall',
-      'TSUninstall',
-      'TSUpdate',
-      'TSUpdateSync',
-      'TSInstallInfo',
-      'TSInstallSync',
-      'TSInstallFromGrammar',
-    },
-    dependencies = {
-      {
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        lazy = true,
-      },
-      {
-        'windwp/nvim-ts-autotag',
-        lazy = true,
-      },
-    },
     config = function()
-      -- treesitter configuration
-      require('nvim-treesitter.configs').setup({
-        -- language parser
-        ensure_installed = {
-          'lua',
-          'python',
-          'terraform',
-          'hcl',
-          'astro',
-          'tsx',
-          'typescript',
-          'javascript',
-          'html',
-          'css',
-          'json',
-        },
-
-        highlight = { enable = true },
-        indent = { enable = true },
-        fold = { enable = true },
-
-        textobjects = {
-          subject = {
-            enable = true,
-            keymaps = {
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-              ['aa'] = '@parameter.outer',
-              ['ia'] = '@parameter.inner',
-              ['ab'] = '@block.outer',
-              ['ib'] = '@block.inner',
-            },
-          },
-        },
-        -- auttag configuration
-        require('nvim-ts-autotag').setup({
-          opts = {
-            enable_close = true,
-            enable_rename = true,
-            enable_close_on_slash = true,
-          },
-        }),
+      require('nvim-treesitter').install({
+        'lua',
+        'python',
+        'terraform',
+        'hcl',
+        'astro',
+        'tsx',
+        'typescript',
+        'javascript',
+        'html',
+        'css',
+        'json',
+        'yaml',
+        'markdown',
+        'markdown_inline',
       })
 
-      -- folding settings
-      vim.opt.foldmethod = 'expr'
-      vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+      -- highlight
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
 
-      -- disable folding by default
-      vim.opt.foldenable = false
-      vim.opt.foldlevel = 99
+      -- folding
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          vim.wo[0][0].foldmethod = 'expr'
+          vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+          vim.wo[0][0].foldenable = false
+        end,
+      })
+
+      -- indent
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          vim.bo.indentexpr = 'v:lua.require\'nvim-treesitter\'.indentexpr()'
+        end,
+      })
     end,
   },
 }
